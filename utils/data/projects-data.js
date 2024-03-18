d263688711bde09042ad6df6ba2921ae59493c8d
yeonjin1357 <yeonjin1357@naver.com>
2024-03-18 19:05:59 +0900

    수정

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,8 +1,3 @@
-import ayla from "/public/image/ayla.jpg";
-import crefin from "/public/image/crefin.jpg";
-import realEstate from "/public/image/real-estate.jpg";
-import travel from "/public/image/travel.jpg";
-
 export const projectsData = [
   {
     id: 1,
@@ -27,17 +22,3 @@ export const projectsData = [
     link: "https://www.notion.so/jinjin98/RealTime-c464bced49bf42da8b41ce898892d517?pvs=4",
   },
 ];
-
-// Do not remove any property.
-// Leave it blank instead as shown below
-
-// {
-//     id: 1,
-//     name: '',
-//     description: "",
-//     tools: [],
-//     role: '',
-//     code: '',
-//     demo: '',
-//     image: crefin,
-// },
--- utils/data/skills.js
+++ utils/data/skills.js
@@ -1,10 +1,5 @@
 export const skillsData = ["HTML", "CSS", "Javascript", "Typescript", "React", "Next JS", "Tailwind", "ViteJS", "MySQL", "Git", "Bootstrap", "Firebase", "MaterialUI"];
 
-// Choose your skills from below. Make sure it's in the same format and spelled correctly.
-// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
-// AVAILABLE SKILLS
-
 /* 
   HTML
   CSS
