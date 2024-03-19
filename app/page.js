b262a4c59c79d0a721e9ccaa725c03600d425f05
yeonjin1357 <yeonjin1357@naver.com>
2024-03-19 09:09:11 +0900

    수정

--- app/page.js
+++ app/page.js
@@ -8,21 +8,21 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-export default async function Home() {
-  async function getData() {
-    // velog 게시물 json 가져오기
-    const res = await fetch(`https://jinjin98.com/public_html/www/articles.json`);
+async function getData() {
+  // velog 게시물 json 가져오기
+  const res = await fetch(`https://jinjin98.com/public_html/www/articles.json`);
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const data = await res.json();
-
-    // thumbnail이 있는 항목만 필터링
-    const filtered = data.filter((item) => item?.thumbnail);
-    return filtered;
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
+  const data = await res.json();
 
+  // thumbnail이 있는 항목만 필터링
+  const filtered = data.filter((item) => item?.thumbnail);
+  return filtered;
+}
+
+export default async function Home() {
   const blogs = await getData();
 
   return (
