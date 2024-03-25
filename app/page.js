b3537cdc628253774695a615b95bcbc34fcc8e40
yeonjin1357 <yeonjin1357@naver.com>
2024-03-25 18:21:15 +0900

    수정

--- app/page.js
+++ app/page.js
@@ -35,7 +35,6 @@ export default async function Home() {
       <Experience />
       <Skills />
       <Projects />
-      {/* <Education /> */}
       <Blog blogs={blogs} />
       <ContactSection />
     </>
