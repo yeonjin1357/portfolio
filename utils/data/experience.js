bf061bc9b2606d5e8bbda74379c4daece254b5c9
yeonjin1357 <yeonjin1357@naver.com>
2024-07-01 21:26:59 +0900

    fix

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: '웹 퍼블리셔 (Web Publisher)',
-    company: '혜윰앤컴퍼니 (Hyeyum Co.)',
+    title: '혜윰앤컴퍼니 (Hyeyum Co.)',
+    company: '웹 퍼블리셔 (Web Publisher)',
     duration: '(2021.11 - 2024.04)',
   },
   {
     id: 2,
-    title: '프론트엔드 개발 (FrontEnd Developer)',
-    company: '엑셈 (Exem)',
+    title: '엑셈 (Exem',
+    company: '프론트엔드 개발 (FrontEnd Developer)',
     duration: '(2024.06 - ing)',
   },
 ];
