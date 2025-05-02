d1294343c504afdbed0ae146212646422f1d7a5b
yeonjin <yeonjin19980125@gmail.com>
2025-05-02 17:58:28 +0900

    fix: 경력 순서 변경 및 코드 정리

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: '혜윰앤컴퍼니 (Hyeyum Co.)',
-    company: '웹 퍼블리셔 (Web Publisher)',
-    duration: '(2021.11 - 2024.04)',
-  },
-  {
-    id: 2,
     title: '엑셈 (Exem)',
     company: '프론트엔드 개발 (FrontEnd Developer)',
     duration: '(2024.06 - ing)',
+  },
+  {
+    id: 2,
+    title: '혜윰앤컴퍼니 (Hyeyum Co.)',
+    company: '웹 퍼블리셔 (Web Publisher)',
+    duration: '(2021.11 - 2024.04)',
   },
 ];
